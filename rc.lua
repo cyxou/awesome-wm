7dc853f76759891e53f5c63c73a15587787e71d1
cyxou <acyxou@gmail.com>
2015-09-23 10:52:28 +0300

    remove mpd player widget

--- rc.lua
+++ rc.lua
@@ -339,7 +339,7 @@ for s = 1, screen.count() do
   local right_layout = wibox.layout.fixed.horizontal()
   if s == 1 then right_layout:add(wibox.widget.systray()) end
   right_layout:add(spr)
-  right_layout:add(mpdwidget)
+  --right_layout:add(mpdwidget)
   --right_layout:add(mailwidget)
   right_layout:add(cpuwidget)
   right_layout:add(memwidget)
