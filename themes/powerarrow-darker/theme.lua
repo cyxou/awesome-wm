989b099c1fb0fe8f4f3714647bd305ec5f2882cb
cyxou <acyxou@gmail.com>
2017-01-23 22:19:01 +0300

    Configure theme with base16-ocean colors

--- themes/powerarrow-darker/theme.lua
+++ themes/powerarrow-darker/theme.lua
@@ -1,28 +1,48 @@
 
 --[[
-                                             
-     Powerarrow Darker Awesome WM config 2.0 
-     github.com/copycat-killer               
-                                             
+
+     Powerarrow Darker Awesome WM config 2.0
+     github.com/copycat-killer
+
 --]]
+
+local color       = {}
+color.base00 = "#2b303b" -- Default Background
+color.base01 = "#343d46" -- Lighter Background (Used for status bars)
+color.base02 = "#4f5b66" -- Selection Background
+color.base03 = "#65737e" -- Comments, Invisibles, Line Highlighting
+color.base04 = "#a7adba" -- Dark Foreground (Used for status bars)
+color.base05 = "#c0c5ce" -- Default Foreground, Caret, Delimiters, Operators
+color.base06 = "#dfe1e8" -- Light Foreground (Not often used)
+color.base07 = "#eff1f5" -- Light Background (Not often used)
+color.base08 = "#bf616a" -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+color.base09 = "#d08770" -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
+color.base0A = "#ebcb8b" -- Classes, Markup Bold, Search Text Background
+color.base0B = "#a3be8c" -- Strings, Inherited Class, Markup Code, Diff Inserted
+color.base0C = "#96b5b4" -- Support, Regular Expressions, Escape Characters, Markup Quotes
+color.base0D = "#8fa1b3" -- Functions, Methods, Attribute IDs, Headings
+color.base0E = "#b48ead" -- Keywords, Storage, Selector, Markup Italic, Diff Changed
+color.base0F = "#ab7967" -- Deprecated, Opening/Closing Embedded Language Tags e.g. <?php ?>
+color.transparent = "#00000000"
 
 local theme                                     = {}
 
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/powerarrow-darker"
-theme.wallpaper                                 = theme.dir .. "/wall.png"
+--theme.wallpaper                                 = theme.dir .. "/wall.png"
+theme.wallpaper                                 = os.getenv("HOME") .. "/Pictures/toronto.jpg"
 
-theme.font                                      = "Terminus 9"
-theme.fg_normal                                 = "#DDDDFF"
-theme.fg_focus                                  = "#EA6F81"
-theme.fg_urgent                                 = "#CC9393"
-theme.bg_normal                                 = "#1A1A1A"
-theme.bg_focus                                  = "#313131"
-theme.bg_urgent                                 = "#1A1A1A"
+theme.font                                      = "Hack 9"
+theme.fg_normal                                 = color.base05
+theme.fg_focus                                  = color.base08
+theme.fg_urgent                                 = color.base05
+theme.bg_normal                                 = color.base00
+theme.bg_focus                                  = color.base02
+theme.bg_urgent                                 = color.base09
 theme.border_width                              = 1
-theme.border_normal                             = "#3F3F3F"
-theme.border_focus                              = "#7F7F7F"
-theme.border_marked                             = "#CC9393"
-theme.tasklist_bg_focus                         = "#1A1A1A"
+theme.border_normal                             = color.base01
+theme.border_focus                              = color.base04
+theme.border_marked                             = color.base0E
+theme.tasklist_bg_focus                         = color.base00
 theme.titlebar_bg_focus                         = theme.bg_focus
 theme.titlebar_bg_normal                        = theme.bg_normal
 theme.titlebar_fg_focus                         = theme.fg_focus
@@ -31,8 +51,12 @@ theme.menu_height                               = 32
 theme.menu_width                                = 240
 theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
 
+theme.taglist_font                              = "Hack 10"
+theme.taglist_bg_focus                          = color.base00
+theme.taglist_fg_focus                          = color.base08
 theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
 theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
+
 theme.layout_tile                               = theme.dir .. "/icons/tile.png"
 theme.layout_tilegaps                           = theme.dir .. "/icons/tilegaps.png"
 theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
